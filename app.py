2ea335b1f9fdc6c219f4a8a1c98e58e405f2069e
dxq <zxc86796139@gmail.com>
2025-12-18 11:31:36 +0800

    refactor: 清理和重構代碼，移除不必要的函數

--- app.py
+++ app.py
@@ -1,45 +1,47 @@
-from flask import Flask, request , jsonify, redirect
+from typing import Literal
+from flask import Flask, request, jsonify, redirect
+import httpx
+
+
 app = Flask(__name__)
-
-
-
-@app.route("/oc", methods=["POST, GET"])
-def return1():
-    oc = request.args.get("oc")  #網址傳參數
-    print(oc)
-
-    request.form.get("oc")  #請求體
-
-
-    print(request.json)
+"""
+    TODO:
+    1. 主統輪尋 GET ESP32 (30s)並暫存資料
+    2. 處理資料並 發送POST封包回 
     
-    
+    待議:
+    1. 是否使用 WebSocket 獲取連線？
+    2. 使否是用 async 框架？
+
+"""
 
 
+def get_esp32() -> tuple[bool, str]:
+    """獲取 ESP32 資料"""
+    esp_url = "http://exp_esp32.test"
+    with httpx.Client(timeout=5.0) as client:
+        req = client.get(esp_url)
+        if req.status_code == httpx.codes.OK:
+            ...  # 處理資料
+            return (True, "Ok")
+        else:
+            return (False, f"status_code = {req.status_code}")
 
-@app.route("/api", methods=["GET"])
-def api():
-    return jsonify(         #.json檔案
-        {
-            "溫度":"oc",
-            "土壤含水量":"wat",
-            "光照度":"lm",
-        }
-    )
 
-#發送東西
-@app.route("/Hello", methods=["GET"])# 網址列
+# 發送東西
+@app.route("/Hello", methods=["GET"])  # 網址列
 def hello():
-    return "<H1>Hello word.</H1>"#輸出的內容
-    
+    return "<H1>Hello word.</H1>"  # 輸出的內容
 
-#接收東西
-@app.route("/post", methods=["POST"])#接收請求
+
+# 接收東西
+@app.route("/post", methods=["POST"])  # 接收請求
 def hello_post():
 
     return f"ok{request.get_data()}"
 
-app.run(host= "0.0.0.0", port=3000)#啟動用
+
+app.run(host="0.0.0.0", port=3000)  # 啟動用
 
 
 """
