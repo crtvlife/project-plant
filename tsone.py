64e30136f4295c87ae0abe510a2897c0d12797b0
jommm2013 <my1060063@gmail.com>
2026-01-20 17:17:23 +0800

    重構 API 端點，移除不必要的註解與變數，簡化資料處理邏輯

--- tsone.py
+++ tsone.py
@@ -1,25 +1,19 @@
 from flask import Flask, request , jsonify , redirect
 app = Flask(__name__)
 
+latest_data = {}
+
 @app.route("/oc", methods=["POST"])
 def return1():
-    #oc = request.args.get("oc")  #網址傳參數
-   # print(oc)
+    global latest_data
     
-    xx = request.form.get("xx")
-    print(xx)
-
-    print(request.form)
+    latest_data = request.get_json()
+    
+    return "OK", 200
 
 
 @app.route("/api", methods=["GET"])
 def api():
-    return jsonify(     #.json檔案
-        {
-            "氣溫":"oc",
-            "土壤含水量":"wat",
-            "光照度":"lm",
-        }
-    )
+   return jsonify(latest_data)
 
 app.run(host= "0.0.0.0", port=3000)
